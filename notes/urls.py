a9c7ef94d2495f9f13eaf33eaea26d204ecf2df5
Pranav Deshmukh <126485418+PranavDeshmukh233@users.noreply.github.com>
2024-04-12 09:01:00 +0530

    #

--- notes/urls.py
+++ notes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('notelist',views.NotesListView.as_view()),
+    path('notelist',views.NotesListView.as_view(),name="notelist"),
     path('note/<int:pk>',views.DetailView.as_view())
 ]
